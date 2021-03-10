57843cc9e66115514c04438bb01406d28ec769b4
heaven <1593623458@qq.com>
2021-03-10 22:09:59 +0800

    后端

---
+++ Java/src/main/java/com/heaven/entity/Movic.java
@@ -0,0 +1,12 @@
+package com.heaven.entity;
+
+import lombok.Data;
+
+@Data
+public class Movic {
+    private Integer id;
+    private String name;
+    private String star;
+    private String time;
+    private Float score;
+}
